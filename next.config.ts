312ada69ef78aaba5e60fe9d6d27bc5e11ee69ae
ravik009 <136836965+ravik009@users.noreply.github.com>
2025-08-01 18:59:07 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin("./src/features/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
